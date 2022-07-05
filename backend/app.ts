a2e8fcb52ec9b016f6eabc539f95ee33223f7156
Auke Steenman <auke.steenman@gmail.com>
2022-07-05 21:44:01 +0200

    Finished devops

--- backend/app.ts
+++ backend/app.ts
@@ -1,8 +1,10 @@
 import express, { Application, Request, Response, NextFunction } from 'express';
 
+const PORT = 3001;
+
 const app: Application = express();
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
   res.send('hello');
 });
 
-app.listen(5000, () => console.log('Express server started'));
+app.listen(PORT, () => console.log('Express server started'));
---
+++ client/src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+	import { Router, Link, Route } from "svelte-navigator";
+	import Home from "./routes/home.svelte";
+  import About from "./routes/about.svelte";
+  import NotFound from "./routes/notFound.svelte";
+</script>
+
+<Router>
+	<nav>
+		<Link to="/">Home</Link>
+		<Link to="about">About</Link>
+	</nav>
+	<div>
+		<Route path="/">
+			<Home />
+		</Route>
+    <Route path="/about">
+			<About />
+		</Route>
+    <Route path="">
+			<NotFound />
+		</Route>
+	</div>
+</Router>
